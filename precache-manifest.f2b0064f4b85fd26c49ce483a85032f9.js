f285621c1b1cc5179a21763e84d0f5cad61ac1c3
Huaiqing <wenhuaiqing@hotmail.com>
2018-10-14 21:51:07 +1000

    Updates

---
+++ precache-manifest.f2b0064f4b85fd26c49ce483a85032f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "8fd298a81361841d67c3",
+    "url": "/contactmanager/static/js/main.8fd298a8.chunk.js"
+  },
+  {
+    "revision": "69b423e27ac620360d6d",
+    "url": "/contactmanager/static/js/1.69b423e2.chunk.js"
+  },
+  {
+    "revision": "8fd298a81361841d67c3",
+    "url": "/contactmanager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "69b423e27ac620360d6d",
+    "url": "/contactmanager/static/css/1.feac1855.chunk.css"
+  },
+  {
+    "revision": "e40293bc14ea5e6ca639b50868030a09",
+    "url": "/contactmanager/index.html"
+  }
+];
